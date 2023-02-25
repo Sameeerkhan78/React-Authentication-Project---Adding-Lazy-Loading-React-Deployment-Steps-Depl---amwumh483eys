4a28c39672a32981c66aeef8583f7e1fd71ee6c9
Sameer Khan <115018276+Sameeerkhan78@users.noreply.github.com>
2023-02-25 12:12:49 +0530

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PostDetail from './PostDetail';
 
-function Profile() {
-    return <div>This is the profile page</div>;
-}
-
-export default Profile;
+const posts = [
+    { name: 'Post 1', path: '/posts/1' },
+    { name: 'Post 2', path: '/posts/2' },
+    { name: 'Post 3', path: '/posts/3' },
+    { name: 'Post 4', path: '/posts/4' }
+];
+export default function PostList() {
+    return (
+      <div className="posts">
+        <h1>Post List</h1>
+        <ul>
+          {/* Render a list of Links from the posts object   */}
+            {posts.map((e,i)=> {
+           return <Link to={e.path} key={i}><li>{e.name}</li></Link>
+          })}
+        </ul>
+      </div>
+    )
+  }
